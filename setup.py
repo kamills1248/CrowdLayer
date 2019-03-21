851791af5fb8df3708989b82344d504cba65afed
Alex Diment <alexdiment@gmail.com>
2019-03-21 17:56:35 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='crowd_layer',
+    python_requires='>=3.6.0',
+    version='0.1dev',
+    packages=['crowd_layer'],
+    license='GNU General Public License v3.0',
+    long_description=open('README.txt').read(),
+)
